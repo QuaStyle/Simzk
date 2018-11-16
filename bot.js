410e2ec171400fcf48cf6724f93bc1162d9038c3
3mk QuaStyle yad <43297529+QuaStyle@users.noreply.github.com>
2018-11-16 01:20:06 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,40 +1,12 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix = "-"
-
+const prefix = "-";
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(` ✨ `,"https://www.twitch.tv/S-F")
-  console.log('')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log(`[Start] ${new Date()}`);
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════]╗')
-  console.log(' Bot Is Online')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
-});
-
-client.on('ready', () => {
-  client.user.setGame(`»Tφp★ .`,'https://www.twitch.tv/v5bz');
+  client.user.setGame('Type *help','https://www.twitch.tv/peery13');
   console.log('---------------');
-  console.log('Desert Bot Is Online')
+  console.log(' Bot Is Online')
   console.log('---------------')
 });
-
 client.on('message', msg => {
 	var prefix = "...=";
   if (msg.author.bot) return;
